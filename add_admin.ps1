7f5e656d5884e4ae35e6bfeb03488ec58bed43f7
Prabhsimran <pswalia2u@gmail.com>
2024-12-17 15:01:14 +0000

    Update add_admin.ps1

--- add_admin.ps1
+++ add_admin.ps1
@@ -13,6 +13,7 @@ Add-LocalGroupMember -Group "Administrators" -Member $username
 
 Write-Output "Local administrator account '$username' created and added to the Administrators group."
 
-net user /add test admin123
+<#net user /add test admin123
 net localgroup "Administrators" test /add
 net localgroup "Remote Desktop Users" test /add 
+#>
